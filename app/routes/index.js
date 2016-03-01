6b8e11315c845dd2b269ff9503c14cc63393eb72
nbhuvaneshwari <nbhuvaneshwari@zeomega.com>
2016-03-01 12:44:37 +0530

    External app to serve json

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-	  return {
-     columns: [
+  ajax: Ember.inject.service(),
+  model() {
+    var d = this.get('ajax').request('/marks');
+    return d;
+    return {
+      columns: [
         ['data1', 30, 200, 100, 400, 150, 250],
         ['data2', 130, 100, 140, 200, 150, 50],
         ['data3', 30,300, 170, 250, 150, 40],
         ['data4', 40,100,170, 150, 150, 20]
-     ],
- //   columns: [
- //     ['data1', 30, 40],
- //     ['data2', 120, 60],
- //   ],
-     type: 'pie',
-     bar: {
-       width: 50
-     }
+      ],
+      type: 'pie',
+      bar: {
+        width: 50
+      }
     };
-	}
+  }
 });
